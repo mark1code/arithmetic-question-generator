254b25af2c5b3358f3b648c235b34e53d10ebc48
passwordpassword-hash <binomialbats@gmail.com>
2024-07-14 15:43:00 +0100

    Add better formatting

--- arithmetic.py
+++ arithmetic.py
@@ -26,15 +26,20 @@ print(f"\nGenerating {num_q} questions with {" ".join(list(set(op)))} sign...\n"
 
 for i in range(num_q):
     sign = random.choice(op)
+    q = ""
     if sign == "+":
-        print(f"{random.randint(100,2000)} {sign} {random.randint(100,1000)} = ")
+        q = f"{random.randint(100,2000):<4} {sign} {random.randint(100,1000):<4} = "
     elif sign == "-":
         x = random.randint(100,2000)
-        print(f"{x} {sign} {getSmaller(x)} =")
-    elif sign == "x":
-        print(f"{random.randint(10, 99)} {sign} {random.randint(10,100)} = ")
+        q = f"{x:<4} {sign} {getSmaller(x):<4} ="
+    elif sign =="x":
+        q = f"{random.randint(10, 99):<4} {sign} {random.randint(10,100):<4} = "
     else:
         divisor = random.randint(3,12)
         quotient = random.randint(13,999)
-        dividend = divisor * quotient
-        print(f"{dividend} {sign} {divisor} = ")
+        divident = divisor * quotient
+        q = f"{divident:<4} {sign} {divisor:<4} = "
+
+    print(f"{str(i+1) + '.':<8} {q}")
+
+
